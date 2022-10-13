f95945b5f42482a3accf92a8545c1210f0cc9521
Mohamed Aziz Gnewi <gnewamedaziz99@gmail.com>
2022-10-13 21:11:09 +0200

    the theme park pipeline

--- createJob.groovy
+++ createJob.groovy
@@ -6,3 +6,17 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/Mohamed-Aziz-Gnewi/themeparktest.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
